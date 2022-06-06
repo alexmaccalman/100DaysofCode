9aff59476c101b6b137816b9401d44833e17e58b
alex.maccalman <alex.maccalman@gmail.com>
2022-06-06 01:43:13 +0000

    day25

--- 25csvdata_pandas/main.py
+++ 25csvdata_pandas/main.py
@@ -1,20 +1,32 @@
-# with open("weather_data.csv") as file:
-#     data = file.readlines()
-
-# import csv
-
-# with open("weather_data.csv") as file:
-#     data = csv.reader(file)
-#     temperature = []
-#     for row in data:
-#         if row[1] != "temp":
-#          temperature.append(int(row[1]))
-
-# print(temperature)
-
 import pandas as pd
 
+# data = pd.read_csv("weather_data.csv")
+# print(type(data))
+#print(data['temp'])
 
+# data_dict = data.to_dict()
+# print(data_dict)
 
-data = pd.read_csv("weather_data.csv")
-print(data)
+# temp_max = data["temp"].max()
+# print(temp_max)
+
+# def convert_to_f(cel):
+#     return (cel * 1.8) +32
+
+# monday = data[data.day == "Monday"]
+# print(convert_to_f(monday.temp))
+
+#create a dataframe frm scratch
+
+# how many grey, red, and black
+# buidl new dataframe
+
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+#print(list(data.columns))
+
+fur = data["Primary Fur Color"].value_counts()
+#fur.rename(['fur color', 'count'])
+fur.index.name = 'fur color'
+fur.name = 'count'
+
+fur.to_csv("squirrel_count.csv")
