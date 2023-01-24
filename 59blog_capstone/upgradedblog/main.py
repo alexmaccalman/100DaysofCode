0c741473de39a73d81f61aa78c905ddf7b17fad1
Alex MacCalman <alex.maccalman@gmail.com>
2023-01-23 22:04:51 -0500

    started 59

---
+++ 59blog_capstone/upgradedblog/main.py
@@ -0,0 +1,19 @@
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
