d7df3be2cdb71a86adf8b2b59479ce363112b5b2
Alex MacCalman <alex.maccalman@gmail.com>
2023-01-27 11:54:43 -0500

    working on 64 still

--- 64website/main.py
+++ 64website/main.py
@@ -9,11 +9,78 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI']= "sqlite:///movie-collection.db"
+# Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.app_context().push()
 
+# CREATE TABLE
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.String(250))
+    description = db.Column(db.String(2000))
+    rating = db.Column(db.Float)
+    ranking = db.Column(db.Integer)
+    review = db.Column(db.String(300))
+    img_url = db.Column(db.String(250)) 
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Movie {self.title}>'
+
+db.create_all()
+
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     )
+# db.session.add(new_movie)
+# db.session.commit()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies = Movie.query.all()
+    return render_template("index.html", movies=all_movies)
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
+
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
+    form = RateMovieForm()
+     
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    print(movie)
+    if form.validate_on_submit():
+       
+        movie.rating = float(form.rating.data)
+        
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", movie=movie, form=form)
+
+@app.route("/delete", methods=['GET', "POST"])
+def delete_movie():
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+# @app.route("/update", methods=['POST'])
+# def update():
 
 
 if __name__ == '__main__':
